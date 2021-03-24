d2506d9c5705c7a1b1c390a48a75a2dee94e7b13
Faaiz K Rissad <faaiz.rissad@wipro.com>
2021-03-24 09:32:30 +0530

    Added Data, Repo and Service

---
+++ OA.Service/IUserProfileService.cs
@@ -0,0 +1,12 @@
+﻿using OA.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OA.Service
+{
+    public interface IUserProfileService
+    {
+        UserProfile GetUserProfile(long id);
+    }
+}

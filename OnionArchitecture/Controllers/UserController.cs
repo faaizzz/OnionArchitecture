757f188e246d846438cb72f9e4c8d4568ee11658
Faaiz K Rissad <faaiz.rissad@wipro.com>
2021-03-24 11:40:05 +0530

    Added User Control and User view model

---
+++ OnionArchitecture/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OA.Data;
+using OA.Service;
+using OnionArchitecture.Models;
+
+namespace OnionArchitecture.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+        private readonly IUserProfileService userProfileService;
+
+        public UserController(IUserService userService, IUserProfileService userProfileService)
+        {
+            this.userService = userService;
+            this.userProfileService = userProfileService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<UserViewModel> model = new List<UserViewModel>();
+            userService.GetUsers().ToList().ForEach(u =>
+            {
+                UserProfile userProfile = userProfileService.GetUserProfile(u.Id);
+                UserViewModel user = new UserViewModel
+                {
+                    Id = u.Id,
+                    Name = $"{userProfile.FirstName} {userProfile.LastName}",
+                    Email = u.Email,
+                    Address = userProfile.Address
+                };
+                model.Add(user);
+            });
+
+            return View(model);
+        }
+    }
+}
--- OnionArchitecture/Startup.cs
+++ OnionArchitecture/Startup.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OA.Repo;
+using OA.Service;
 
 namespace OnionArchitecture
 {
@@ -24,6 +27,10 @@ namespace OnionArchitecture
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),b=>b.MigrationsAssembly("OnionArchitecture")));
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IUserProfileService, UserProfileService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
